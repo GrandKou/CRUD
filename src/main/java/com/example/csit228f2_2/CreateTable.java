75d4118cef39b30c4478bca00866ab0c35f797fa
GrandKou <rieub123@gmail.com>
2024-04-24 00:35:19 +0800

    two table yes

--- src/main/java/com/example/csit228f2_2/CreateTable.java
+++ src/main/java/com/example/csit228f2_2/CreateTable.java
@@ -5,11 +5,23 @@ public class CreateTable {
     public static void main(String[] args) {
         try (Connection c = MySqlConnection.getConnection();
             Statement statement = c.createStatement()){
+
+            //tabla de usuarios si
             String createTableQuery = "CREATE TABLE IF NOT EXISTS users (" +
                     "id INT AUTO_INCREMENT PRIMARY KEY," +
                     "name VARCHAR(50) NOT NULL," +
                     "password VARCHAR(50) NOT NULL)";
             statement.execute(createTableQuery);
+
+            //tabla de notas si
+            String createNotesTableQuery = "CREATE TABLE IF NOT EXISTS notes (" +
+                    "id INT AUTO_INCREMENT PRIMARY KEY," +
+                    "userid INT," +
+                    "title VARCHAR(50) NOT NULL," +
+                    "content VARCHAR(100) NOT NULL," +
+                    "FOREIGN KEY (userid) REFERENCES users(id))";
+            statement.executeUpdate(createNotesTableQuery);
+
             System.out.println("Table created successfully");
         } catch (SQLException e) {
             e.printStackTrace();
